5e5104953f485f346f80bdf48afd220066ed8518
OArreola <orlando_arreola@hotmail.com>
2020-05-19 12:15:33 -0700

    Added blur for events and some clean up

--- js/scripts.js
+++ js/scripts.js
@@ -133,92 +133,106 @@ let createOrder = function () {
     validateCCLuhnFormula,
     updateCompletedOrder;
 
-  validateDeliveryInfo = function () {
+  validateDeliveryInfo = function (item) {
+    let name = $("#deliveryName").val();
+    let addrType = $("#deliveryAddrType").val();
+    let addrTypeOther = $("#deliveryAddrTypeOther").val();
+    let address = $("#deliveryAddress").val();
+    let city = $("#deliveryCity").val();
+    let state = $("#deliveryState").val();
+    let zip = $("#deliveryZip").val();
+    let phone = $("#deliveryPhone").val();
+    let email = $("#deliveryEmail").val();
+
     deliveryValid = true;
 
-    //Validate name
-    $("#deliveryName").removeClass("is-valid").removeClass("is-invalid");
-    let name = $("#deliveryName").val();
-    if (validateTextOnly(name)) $("#deliveryName").addClass("is-valid");
-    else {
-      $("#deliveryName").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryName") {
+      $("#deliveryName").removeClass("is-valid").removeClass("is-invalid");
+      if (validateTextOnly(name)) $("#deliveryName").addClass("is-valid");
+      else {
+        $("#deliveryName").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
-    //Validate address type
-    $("#deliveryAddrType").removeClass("is-valid").removeClass("is-invalid");
-    let addrType = $("#deliveryAddrType").val();
-    if (addrType !== "0") $("#deliveryAddrType").addClass("is-valid");
-    else {
-      $("#deliveryAddrType").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryAddrType") {
+      $("#deliveryAddrType").removeClass("is-valid").removeClass("is-invalid");
+      if (addrType !== "0") $("#deliveryAddrType").addClass("is-valid");
+      else {
+        $("#deliveryAddrType").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
-    //Validate address type other
-    $("#deliveryAddrTypeOther").removeClass("is-valid").removeClass("is-invalid");
-    let addrTypeOther = $("#deliveryAddrTypeOther").val();
-    if (validateTextOnly(addrTypeOther)) $("#deliveryAddrTypeOther").addClass("is-valid");
-    else {
-      $("#deliveryAddrTypeOther").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryAddrTypeOther") {
+      if (addrType === "7") {
+        //Validate address type other only if address type = other
+        $("#deliveryAddrTypeOther").removeClass("is-valid").removeClass("is-invalid");
+        if (validateTextOnly(addrTypeOther)) $("#deliveryAddrTypeOther").addClass("is-valid");
+        else {
+          $("#deliveryAddrTypeOther").addClass("is-invalid");
+          deliveryValid = false;
+        }
+      }
     }
 
-    //Validate address
-    $("#deliveryAddress").removeClass("is-valid").removeClass("is-invalid");
-    let address = $("#deliveryAddress").val();
-    if (address.length > 0) $("#deliveryAddress").addClass("is-valid");
-    else {
-      $("#deliveryAddress").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryAddress") {
+      $("#deliveryAddress").removeClass("is-valid").removeClass("is-invalid");
+      if (address.length > 0) $("#deliveryAddress").addClass("is-valid");
+      else {
+        $("#deliveryAddress").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
-    //Validate city
-    $("#deliveryCity").removeClass("is-valid").removeClass("is-invalid");
-    let city = $("#deliveryCity").val();
-    if (validateTextOnly(city)) $("#deliveryCity").addClass("is-valid");
-    else {
-      $("#deliveryCity").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryCity") {
+      $("#deliveryCity").removeClass("is-valid").removeClass("is-invalid");
+      if (validateTextOnly(city)) $("#deliveryCity").addClass("is-valid");
+      else {
+        $("#deliveryCity").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
-    //Validate state
-    $("#deliveryState").removeClass("is-valid").removeClass("is-invalid");
-    let state = $("#deliveryState").val();
-    if (validateState(state)) $("#deliveryState").addClass("is-valid");
-    else {
-      $("#deliveryState").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryState") {
+      $("#deliveryState").removeClass("is-valid").removeClass("is-invalid");
+      if (validateState(state)) $("#deliveryState").addClass("is-valid");
+      else {
+        $("#deliveryState").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
-    //Validate zip
-    $("#deliveryZip").removeClass("is-valid").removeClass("is-invalid");
-    let zip = $("#deliveryZip").val();
-    if (validateZip(zip)) $("#deliveryZip").addClass("is-valid");
-    else {
-      $("#deliveryZip").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryZip") {
+      //Validate zip
+      $("#deliveryZip").removeClass("is-valid").removeClass("is-invalid");
+      if (validateZip(zip)) $("#deliveryZip").addClass("is-valid");
+      else {
+        $("#deliveryZip").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
-    //Validate phone
-    $("#deliveryPhone").removeClass("is-valid").removeClass("is-invalid");
-    let phone = $("#deliveryPhone").val();
-    if (validatePhoneNumber(phone)) $("#deliveryPhone").addClass("is-valid");
-    else {
-      $("#deliveryPhone").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryPhone") {
+      $("#deliveryPhone").removeClass("is-valid").removeClass("is-invalid");
+      if (validatePhoneNumber(phone)) $("#deliveryPhone").addClass("is-valid");
+      else {
+        $("#deliveryPhone").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
-    //Validate email
-    $("#deliveryEmail").removeClass("is-valid").removeClass("is-invalid");
-    let email = $("#deliveryEmail").val();
-    if (validateEmail(email)) $("#deliveryEmail").addClass("is-valid");
-    else {
-      $("#deliveryEmail").addClass("is-invalid");
-      deliveryValid = false;
+    if (item === undefined || item === "deliveryEmail") {
+      $("#deliveryEmail").removeClass("is-valid").removeClass("is-invalid");
+      if (validateEmail(email)) $("#deliveryEmail").addClass("is-valid");
+      else {
+        $("#deliveryEmail").addClass("is-invalid");
+        deliveryValid = false;
+      }
     }
 
     // deliveryValid = true;
-    if (deliveryValid) {
+    if (item === undefined && deliveryValid === true) {
       $("#frmDelivery").hide();
       $("#frmBuildOrder").show();
     }
@@ -277,7 +291,6 @@ let createOrder = function () {
       $("input[name='toppings']:checked").each(function () {
         toppingsCount++;
         toppingsStr = toppingsStr + $(this).val() + ", ";
-        // window.console.log($(this).val());
       });
       totalCharge += 0.99 * toppingsCount;
     }
@@ -311,100 +324,103 @@ let createOrder = function () {
       $("#billingZip").val("");
     }
   };
-  validateBillingInfo = function () {
+  validateBillingInfo = function (item) {
     let validExpiration = true;
+    let name = $("#billingName").val();
+    let address = $("#billingAddress").val();
+    let city = $("#billingCity").val();
+    let state = $("#billingState").val();
+    let zip = $("#billingZip").val();
+    let ccmonth = $("#ccExpMonth").val();
+    let ccyear = $("#ccExpYear").val();
     billingValid = true;
 
-    //Validate name
-    $("#billingName").removeClass("is-valid").removeClass("is-invalid");
-    let name = $("#billingName").val();
-    if (validateTextOnly(name)) $("#billingName").addClass("is-valid");
-    else {
-      $("#billingName").addClass("is-invalid");
-      billingValid = false;
+    if (item === undefined || item === "billingName") {
+      $("#billingName").removeClass("is-valid").removeClass("is-invalid");
+      if (validateTextOnly(name)) $("#billingName").addClass("is-valid");
+      else {
+        $("#billingName").addClass("is-invalid");
+        billingValid = false;
+      }
     }
 
-    //Validate address
-    $("#billingAddress").removeClass("is-valid").removeClass("is-invalid");
-    let address = $("#billingAddress").val();
-    if (address.length > 0) $("#billingAddress").addClass("is-valid");
-    else {
-      $("#billingAddress").addClass("is-invalid");
-      billingValid = false;
+    if (item === undefined || item === "billingAddress") {
+      $("#billingAddress").removeClass("is-valid").removeClass("is-invalid");
+      if (address.length > 0) $("#billingAddress").addClass("is-valid");
+      else {
+        $("#billingAddress").addClass("is-invalid");
+        billingValid = false;
+      }
     }
 
-    //Validate city
-    $("#billingCity").removeClass("is-valid").removeClass("is-invalid");
-    let city = $("#billingCity").val();
-    if (validateTextOnly(city)) $("#billingCity").addClass("is-valid");
-    else {
-      $("#billingCity").addClass("is-invalid");
-      billingValid = false;
+    if (item === undefined || item === "billingCity") {
+      $("#billingCity").removeClass("is-valid").removeClass("is-invalid");
+      if (validateTextOnly(city)) $("#billingCity").addClass("is-valid");
+      else {
+        $("#billingCity").addClass("is-invalid");
+        billingValid = false;
+      }
     }
 
-    //Validate state
-    $("#billingState").removeClass("is-valid").removeClass("is-invalid");
-    let state = $("#billingState").val();
-    if (validateState(state)) $("#billingState").addClass("is-valid");
-    else {
-      $("#billingState").addClass("is-invalid");
-      billingValid = false;
+    if (item === undefined || item === "billingState") {
+      $("#billingState").removeClass("is-valid").removeClass("is-invalid");
+      if (validateState(state)) $("#billingState").addClass("is-valid");
+      else {
+        $("#billingState").addClass("is-invalid");
+        billingValid = false;
+      }
     }
 
-    //Validate zip
-    $("#billingZip").removeClass("is-valid").removeClass("is-invalid");
-    let zip = $("#billingZip").val();
-    if (validateZip(zip)) $("#billingZip").addClass("is-valid");
-    else {
-      $("#billingZip").addClass("is-invalid");
-      billingValid = false;
+    if (item === undefined || item === "billingZip") {
+      $("#billingZip").removeClass("is-valid").removeClass("is-invalid");
+      if (validateZip(zip)) $("#billingZip").addClass("is-valid");
+      else {
+        $("#billingZip").addClass("is-invalid");
+        billingValid = false;
+      }
     }
 
-    //Validate Expiration Month
-    $("#ccExpMonth").removeClass("is-valid").removeClass("is-invalid");
-    let ccmonth = $("#ccExpMonth").val();
-    if (ccmonth !== null) $("#ccExpMonth").addClass("is-valid");
-    else {
-      $("#ccExpMonth").addClass("is-invalid");
-      billingValid = false;
-      validExpiration = false;
-    }
-
-    //Validate Expiration Year
-    $("#ccExpYear").removeClass("is-valid").removeClass("is-invalid");
-    let ccyear = $("#ccExpYear").val();
-    if (ccyear !== null) $("#ccExpYear").addClass("is-valid");
-    else {
-      $("#ccExpYear").addClass("is-invalid");
-      billingValid = false;
-      validExpiration = false;
-    }
-
-    //Validate Current Date
-    let today = new Date();
-    if (validExpiration) {
-      if (Number(ccyear) > today.getFullYear()) {
-      } else if (Number(ccyear) === today.getFullYear()) {
-        window.console.log("Same year");
-        if (Number(ccmonth) >= today.getMonth()) {
-        } else {
-          $("#ccExpMonth").next().text("Date is in the past");
-          $("#ccExpMonth").removeClass("is-valid").addClass("is-invalid");
-          $("#ccExpYear").next().text("Date is in the past");
-          $("#ccExpYear").removeClass("is-valid").addClass("is-invalid");
+    if (item === undefined || item === "ccExpMonth" || item === "ccExpYear") {
+      $("#ccExpMonth").removeClass("is-valid").removeClass("is-invalid");
+      if (ccmonth !== null) $("#ccExpMonth").addClass("is-valid");
+      else {
+        $("#ccExpMonth").addClass("is-invalid");
+        billingValid = false;
+        validExpiration = false;
+      }
+      $("#ccExpYear").removeClass("is-valid").removeClass("is-invalid");
+      if (ccyear !== null) $("#ccExpYear").addClass("is-valid");
+      else {
+        $("#ccExpYear").addClass("is-invalid");
+        billingValid = false;
+        validExpiration = false;
+      }
+      let today = new Date();
+      if (validExpiration) {
+        if (Number(ccyear) > today.getFullYear()) {
+        } else if (Number(ccyear) === today.getFullYear()) {
+          // window.console.log("Same year");
+          if (Number(ccmonth) >= today.getMonth()) {
+          } else {
+            $("#ccExpMonth").next().text("Date is in the past");
+            $("#ccExpMonth").removeClass("is-valid").addClass("is-invalid");
+            $("#ccExpYear").next().text("Date is in the past");
+            $("#ccExpYear").removeClass("is-valid").addClass("is-invalid");
+          }
         }
       }
     }
 
-    //Validate CVV
-    $("#ccCVV").removeClass("is-valid").removeClass("is-invalid");
-    let cvv = $("#ccCVV").val();
-    if (validateCVV(cvv)) $("#ccCVV").addClass("is-valid");
-    else {
-      $("#ccCVV").addClass("is-invalid");
-      billingValid = false;
+    if (item === undefined || item === "ccCVV") {
+      //Validate CVV
+      $("#ccCVV").removeClass("is-valid").removeClass("is-invalid");
+      let cvv = $("#ccCVV").val();
+      if (validateCVV(cvv)) $("#ccCVV").addClass("is-valid");
+      else {
+        $("#ccCVV").addClass("is-invalid");
+        billingValid = false;
+      }
     }
-
     // billingValid = true;
   };
   validateCCNumber = function () {
@@ -418,7 +434,6 @@ let createOrder = function () {
       let prefix = Number(ccNumberStr.slice(0, 1));
       if (prefix === 4 && (ccNumberStr.length === 13 || ccNumberStr.length === 16)) {
         cctype = ccTypeVisa;
-        // window.console.log("cctype" + cctype);
       } else {
         prefix = Number(ccNumberStr.slice(0, 2));
         if (prefix === 37 && ccNumberStr.length === 15) {
@@ -470,7 +485,6 @@ let createOrder = function () {
   };
 
   updateCompletedOrder = function () {
-    window.console.log("Here");
     $("#completeDough").text($("#totalDough").text());
     $("#completeSize").text($("#totalSize").text());
     $("#completeCheese").text($("#totalCheese").text());
@@ -488,8 +502,8 @@ let createOrder = function () {
       sizesInfo = sizesInfoOptions;
       return this;
     },
-    validateDelivery: function () {
-      validateDeliveryInfo();
+    validateDelivery: function (item) {
+      validateDeliveryInfo(item);
       return this;
     },
     updateBuild: function (evt) {
@@ -504,11 +518,13 @@ let createOrder = function () {
       updateBilling();
       return this;
     },
-    validateBilling: function () {
-      validateBillingInfo();
-      validateCCNumber();
-      if (billingCardValid) validateCCLuhnFormula();
-      if (billingValid) {
+    validateBilling: function (item) {
+      validateBillingInfo(item);
+      if (item === undefined || item === "billingCCNumber") {
+        validateCCNumber();
+        if (billingCardValid) validateCCLuhnFormula();
+      }
+      if (item === undefined && billingValid === true) {
         updateCompletedOrder();
         $("#orderingForm").hide();
         $("#orderComplete").show();
@@ -526,6 +542,11 @@ $(function () {
   $("#btnDeliveryNext").on("click", function () {
     pizzaOrder.validateDelivery();
   });
+
+  $(".frmDeliveryItem").blur(function (evt) {
+    pizzaOrder.validateDelivery(evt.target.id);
+  });
+
   $("#deliveryAddrType").change(function () {
     if ($("#deliveryAddrType").val() === "7") {
       $("#delAddTypeOther").show();
@@ -554,6 +575,9 @@ $(function () {
     $("#frmBilling").show();
   });
 
+  $(".frmBillingItem").blur(function (evt) {
+    pizzaOrder.validateBilling(evt.target.id);
+  });
   $("#btnConfirm").on("click", function () {
     pizzaOrder.validateBilling();
   });
